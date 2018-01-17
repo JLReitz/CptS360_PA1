cae7a70dd075276f6f81768e928b307b41737f68
Jensen Reitz <JLReitz@live.com>
2018-01-16 16:33:55 -0800

    Updating PA1 files

--- PA1.c
+++ PA1.c
@@ -4,9 +4,23 @@
   Programming Assingment 1
  */
 
-#include PA1.h
 
-int main(int argc, char *argv, char *env)
+#include "PA1.h"
+
+#define DEBUG_PRINTS
+
+int main(int argc, char * argv[], char * env[])
 {
-  return 0;
+#ifdef DEBUG_PRINTS
+	if(argc > 1)
+	{
+		printf("Line printed by prints(): ");
+		prints(argv[1]);
+		printf("\n");
+	}
+	else
+		printf("Enter a string to be reprinted\n");
+#endif
+	
+	return 0;
 }
--- PA1.h
+++ PA1.h
@@ -1,4 +1,44 @@
 #ifndef PA1_H
 #define PA1_H
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+typedef unsigned int u32;
+
+// Global Variables ********************************************************************************
+
+const int _Hex_Base = 6, _Oct_Base = 8, _Dec_Base = 10;
+const char * _ctable = "0123456789ABCDEF";
+
+// Function Definitions ****************************************************************************
+
+int rpu(u32 x)
+{  
+    char c;
+    if (x){
+       c = _ctable[x % BASE];
+       rpu(x / D_BASE);
+       putchar(c);
+    }
+}
+
+void prints(char * s)
+{
+	int i = 0;
+	
+	while(*(s+i))
+		putchar(*(s+i));
+}
+
+int printu(u32 x)
+{
+   (x==0)? putchar('0') : rpu(x);
+   putchar(' ');
+}
+
+
 #endif
